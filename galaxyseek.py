2fdbae3038c3cfc14019bab38dee526551c5d726
rjansen1984 <rick.jansen1984@gmail.com>
2018-11-14 16:05:33 +0100

    Start Galaxy workflow after uploading data files.

--- galaxyseek.py
+++ galaxyseek.py
@@ -1,12 +1,90 @@
 import json
 import subprocess
 import sys
+import time
 
 from subprocess import call
 from bioblend.galaxy import GalaxyInstance
 
-url = input("Ënter the Galaxy server URL: ")  # URL of the Galaxy instance
-key = input("Enter Galaxy API key: ") # Galaxy API key
+url = sys.argv[3]  # URL of the Galaxy instance
+key = sys.argv[4]  # Galaxy API key
+
+
+def get_input_data():
+    """Get input data based on the selected history.
+    Find the number of uploaded files and return the id's of the files.
+
+    Arguments:
+        galaxyemail: The Galaxy email address.
+        galaxypass: The Galaxy password.
+        server: The Galaxy server URL.
+
+    Returns:
+        A list of input files from the Galaxy history and
+        the amount of input datasets in the history.
+    """
+    gi = get_galaxy_instance(url, key)
+    historyid = get_history_id(url, key)
+    hist_contents = gi.histories.show_history(historyid, contents=True)
+    inputs = {}
+    datacount = 0
+    datasets = [dataset for dataset in hist_contents if not dataset['deleted']]
+    for dataset in datasets:
+        inputs[dataset['name']] = dataset['id']
+        datacount += 1
+    return inputs, datacount
+
+
+def run_galaxy_workflow(historyid, workflowid, gi):
+    """Run a Galaxy workflow.
+    
+    Arguments:
+        historyid: Galaxy history id with the input data files.
+        workflowid: The id of the Galaxy workflow.
+        gi: The Galaxy instance.
+    """
+    datamap = dict()
+    mydict = {}
+    jsonwf = gi.workflows.export_workflow_json(workflowid)
+    for i in range(len(jsonwf["steps"])):
+        if jsonwf["steps"][str(i)]["name"] == "Input dataset":
+            try:
+                label = jsonwf["steps"][str(i)]["inputs"][0]["name"]
+            except IndexError:
+                label = jsonwf["steps"][str(i)]["label"]
+            mydict[label] = gi.workflows.get_workflow_inputs(
+                workflowid, label=label)[0]
+    in_count = 0
+    for k, v in mydict.items():
+        datasets = get_input_data()[0]
+        for dname, did in datasets.items():
+            if k in dname:
+                datamap[v] = {'src': "hda", 'id': did}
+        in_count += 1
+    hist = gi.histories.show_history(historyid)
+    state = hist['state_ids']
+    dump = json.dumps(state)
+    status = json.loads(dump)
+    # Stop process after workflow is done
+    while (
+            status['running'] or
+            status['queued'] or
+            status['new'] or
+            status['upload']
+    ):
+        time.sleep(10)
+        hist = gi.histories.show_history(historyid)
+        state = hist['state_ids']
+        dump = json.dumps(state)
+        status = json.loads(dump)
+        if (
+                not status['running'] and
+                not status['queued'] and
+                not status['new'] and
+                not status['upload']
+        ):
+            gi.workflows.run_workflow(
+                workflowid, datamap, history_id=historyid)
 
 
 def get_history_id(url, key):
@@ -31,7 +109,7 @@ def get_history_id(url, key):
 def send_data_files(assayname, file):
     """Create a new history with the assay name and 
     send the data files to the Galaxy server.
-    
+
     Arguments:
         file: List of files to send to Galaxy.
         historyid: The history to send the files to.
@@ -47,12 +125,12 @@ def send_data_files(assayname, file):
 
 def get_galaxy_instance(url, key):
     """Create a new Galaxy instance.
-    
+
     Arguments:
         url: The Galaxy URL.
         email: Email address used on the Galaxy server.
         password: Password used on the Galaxy server.
-    
+
     Returns:
         The Galaxy instance.
     """
@@ -64,6 +142,12 @@ def get_galaxy_instance(url, key):
 
 if __name__ == "__main__":
     gi = get_galaxy_instance(url, key)
-    file = sys.argv[1] # List with data files to send to Galaxy.
+    file = sys.argv[1]  # List with data files to send to Galaxy.
     assayname = str(sys.argv[2])
     send_data_files(assayname, file)
+    historyid = get_history_id(url, key)
+    workflows = gi.workflows.get_workflows(name="TestCase_AML")
+    print(workflows)
+    for workflow in workflows:
+        workflowid = workflow['id']
+    run_galaxy_workflow(historyid, workflowid, gi)
